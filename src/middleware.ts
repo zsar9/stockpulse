d0d5a1944f0b59cd5184a26830f0a8531b284ca4
zsar9 <42940344+zsar9@users.noreply.github.com>
2025-08-17 12:16:27 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,3 +5,8 @@ export function middleware(req: NextRequest) {
   // You can add auth, redirects, headers, etc. here.
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+};
+
