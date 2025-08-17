108d31a22d3eeedba17a2f98ed26c31b97810044
zsar9 <42940344+zsar9@users.noreply.github.com>
2025-08-17 12:13:43 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,7 @@
-export { };
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  // You can add auth, redirects, headers, etc. here.
+  return NextResponse.next();
+}
